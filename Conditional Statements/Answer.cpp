6a213929a84b21b4d09c1887fe2bda359fcd061e
Nikhil V Gopal <70807254+nikhilv2@users.noreply.github.com>
2021-05-26 09:58:44 +0530

    Create Answer.cpp

---
+++ Conditional Statements/Answer.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(n==1){
+        cout<<"one\n";
+    }
+    if(n==2){
+        cout<<"two\n";
+    }
+    if(n==3){
+        cout<<"three\n";
+    }
+    if(n==4){
+        cout<<"four\n";
+    }
+    if(n==5){
+        cout<<"five\n";
+    }
+    if(n==6){
+        cout<<"six\n";
+    }
+    if(n==7){
+        cout<<"seven\n";
+    }
+    if(n==8){
+        cout<<"eight\n";
+    }
+    if(n==9){
+        cout<<"nine\n";
+    }
+    if(n>9){
+        cout<<"Greater than 9\n";
+    }
+
+
+
+    // Write Your Code Here
+
+    return 0;
+}
