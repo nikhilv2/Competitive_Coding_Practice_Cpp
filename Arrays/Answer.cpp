b719d320e714b48668219cdf0cc5034544b1f66a
Nikhil V Gopal <nikhilvenu2000@gmail.com>
2021-05-29 21:25:07 +0530

    Create Answer.cpp

---
+++ Arrays/Answer.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a[100000];
+    int n, i;
+    cin>>n;
+    for(i = 1; i<= n; i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        cout<<a[n-i]<<" ";
+    }
+    return 0;
+}
